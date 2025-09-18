1b768f737f217a15df9b1069640374bbcbdfcd9e
Edwin <emuno1078@gmail.com>
2025-09-17 20:38:24 -0500

    feat(tabs): added bottom tab navigation with icons (Home, Closet, Outfits)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,38 @@
 import { Tabs } from "expo-router";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ headerTitleAlign: "center" }}>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="items" options={{ title: "Items" }} />
-      <Tabs.Screen name="outfits" options={{ title: "Outfits" }} />
+      <Tabs.Screen name="index" 
+      options={{ 
+        title: "Home", tabBarIcon:({color,size}) =>(
+        <Ionicons name = "home" size={size} color={color} />
+            ), 
+          }} 
+        />
+
+
+      <Tabs.Screen name="items" 
+      options={{ 
+        title: "Closet", 
+        tabBarIcon: ({ color, size }) => (
+        <MaterialCommunityIcons name = "hanger" size = {size} color={color} />
+              ),
+            }}
+          />
+
+
+      <Tabs.Screen name="outfits" 
+      options={{ 
+        title: "Outfits",
+        tabBarIcon: ({color, size}) => (
+          <Ionicons name = "shirt-outline" size={size} color={color} />
+              ),
+            }} 
+         />
+
+         
     </Tabs>
   );
 }
